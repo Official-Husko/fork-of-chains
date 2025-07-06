a0bde437a4f1155d6a334ef11b79e64ee7780d17
Official-Husko <official.husko@gmail.com>
2025-07-07 01:15:58 +0200

    style: Fix formatting and add missing semicolons in AAA_twineclass.ts

--- src/scripts/AAA_twineclass.ts
+++ src/scripts/AAA_twineclass.ts
@@ -1,18 +1,15 @@
-// @ts-nocheck
-
 // To support saving and reloading classes in twine save games.
 // All classes should inherit from this or else implement a .clone() and .toJSON()
 // Even if you're not planning to save it into the variables, still preferred to inherit from this
 // in case they got included somewhere
 setup.TwineClass = class TwineClass {
   clone() {
-    return setup.rebuildClassObject(this.constructor, this)
+    return setup.rebuildClassObject(this.constructor, this);
   }
   toJSON() {
-    return setup.toJsonHelper(this.constructor.name, this)
+    return setup.toJsonHelper(this.constructor.name, this);
   }
-}
-
+};
 
 // If your class is not defined in `setup`, use this.
 setup.TwineClassCustom = class TwineClassCustom {
@@ -20,16 +17,14 @@ setup.TwineClassCustom = class TwineClassCustom {
    * @returns {string}   // e.g., setup.a.b.c if your class is setup.a.b.c.Class()
    */
   getContainer() {
-    return `setup`
+    return `setup`;
   }
 
   clone() {
-    return setup.rebuildClassObject(this.constructor, this)
+    return setup.rebuildClassObject(this.constructor, this);
   }
 
   toJSON() {
-    return setup.toJsonHelper(this.constructor.name, this, this.getContainer())
+    return setup.toJsonHelper(this.constructor.name, this, this.getContainer());
   }
-}
-
-
+};
